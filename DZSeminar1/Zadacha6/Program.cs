3c182e907a5614345267f569ec0161e51e2c6201
Anatoly <anatoly.shirshov1989@gmail.com>
2022-12-18 14:58:16 +1000

    Sixth exercise

---
+++ DZSeminar1/Zadacha6/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 6. Показать вторую цифру трёхзначного числа.
+
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a / 10 % 10;
+Console.Write("Вторая цифра числа: ");
+Console.WriteLine(b);
