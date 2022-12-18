d74924e72d1e6f45ae31220647c164a785f92b6e
Anatoly <anatoly.shirshov1989@gmail.com>
2022-12-18 14:42:08 +1000

    Fifth exercise

---
+++ DZSeminar1/Zadacha5/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 5. Показать последнюю цифру трёхзначного числа.
+
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a % 10;
+Console.Write("Последняя цифра числа: ");
+Console.WriteLine(b);
