246e3055e81cdf3fbb7ec6d89237d9215d906892
Anatoly <anatoly.shirshov1989@gmail.com>
2022-12-18 15:07:17 +1000

    Seventh exercise

---
+++ DZSeminar1/Zadacha7/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 7. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.
+
+int a = new Random().Next(10, 99);
+Console.Write("Число: ");
+Console.WriteLine(a);
+
+if (a > 10 && a < 99)
+{
+    if (a / 10 > a % 10)
+    {
+        Console.WriteLine("Первая цифра числа больше");
+    }
+    else
+    if (a / 10 == a % 10)
+    {
+        Console.WriteLine("Цифры одинаковы");
+    }
+    else
+        Console.WriteLine("Вторая цифра числа больше");
+}
