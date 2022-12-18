4ba359b3c0120353fc783726c8f186723dc315a4
Anatoly <anatoly.shirshov1989@gmail.com>
2022-12-18 13:55:25 +1000

    First exercise

---
+++ DZSeminar1/Zadacha1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1. По двум заданным числам проверять является ли первое квадратом второго.
+
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+if (numberB * numberB == numberA)
+{
+    Console.WriteLine("Первое число является квадратом второго.");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго.");
+}
---
+++ DZSeminar2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// Console.Write("Введите число -> ");
+// int number = int.Parse(Console.ReadLine()!);
+// string numberString = $"{number}";
+// int lenght = numberString.Length;
+
+// if (lenght >= 3 )
+// {
+//     Console.Write(numberString[2]);
+// }
+// else
+//     {
+//         Console.Write("третьей цифры нет");
+//     }
