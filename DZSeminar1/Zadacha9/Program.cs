af228818c10b0b6b9c8ec246be189b8acd396caa
Anatoly <anatoly.shirshov1989@gmail.com>
2022-12-20 15:37:28 +1000

    Ninth exercise

---
+++ DZSeminar1/Zadacha9/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 9. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Введите первое число: ");
+Double FN = Double.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+Double SN = Double.Parse(Console.ReadLine()!);
+
+
+if (FN % SN == 0)
+{
+    Console.Write("Первое число кратно второму");
+}
+else
+{
+    Console.WriteLine(FN / SN);
+    Console.WriteLine("Число не кратно");
+}
