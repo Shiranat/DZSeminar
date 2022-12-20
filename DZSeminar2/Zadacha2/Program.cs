b51676d30867b6c5af2e6cea9844943a758e9c31
Anatoly <anatoly.shirshov1989@gmail.com>
2022-12-20 16:11:53 +1000

    second exercise

---
+++ DZSeminar2/Zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+string numberString = $"{number}";
+int lenght = numberString.Length;
+
+if (lenght >= 3 )
+{
+    Console.Write("Третья цифра числа: ");
+    Console.Write(numberString[2]);
+}
+else
+    {
+        Console.Write("третьей цифры нет");
+    }
