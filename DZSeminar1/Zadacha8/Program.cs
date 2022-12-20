43d0d2754bf2e17a270a0299881593bb1bb6d003
Anatoly <anatoly.shirshov1989@gmail.com>
2022-12-20 15:32:23 +1000

    Еighth exercise

---
+++ DZSeminar1/Zadacha8/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 8. Удалить вторую цифру трёхзначного числа.
+
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a / 100 * 10 + a % 10;
+Console.WriteLine(b);
