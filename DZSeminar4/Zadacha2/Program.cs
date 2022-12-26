3ecdfb4c0a3677ba0bf6c0eba3e096b1cd1cda41
Anatoly <anatoly.shirshov1989@gmail.com>
2022-12-27 05:47:28 +1000

    Second exercise

--- DZSeminar4/Zadacha2/Program.cs
+++ DZSeminar4/Zadacha2/Program.cs
@@ -1,10 +1,18 @@
-﻿// Таблица умножения
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
 
-for (int i = 2; i <= 10; i++)
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+string numberString = $"{number}";
+int lenght = numberString.Length;
+int Sum = 0;
+
+for (int i = 0; i < lenght; i++)
 {
-    for (int j = 1; j <= 10; j++)
-    {
-        Console.WriteLine($"{i} x {j} = {i * j}");
-    }
-    Console.WriteLine();
+    string a = $"{numberString[i]}";
+    Sum = Sum + Convert.ToInt32(a);
 }
+string str = $"Сумма цифр в числе {number} = {Sum}";
+Console.WriteLine(str);
