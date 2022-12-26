3d938d47dc8c1b6128abd91c1c1b144740d4a459
Anatoly <anatoly.shirshov1989@gmail.com>
2022-12-27 01:25:29 +1000

    First exercise

---
+++ DZSeminar4/Zadacha1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+// Не используя Math.Pow!
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+int result = 1;
+
+for (int i = 1; i <= B; i++)
+{
+    result = result * A;
+}
+string str = $"Число {A} в степени {B} = {result}";
+Console.WriteLine(str);
