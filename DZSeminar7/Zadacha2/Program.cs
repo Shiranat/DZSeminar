b3d7cfe8712039ef4ed56f57f01b356562d4f84a
Anatoly <anatoly.shirshov1989@gmail.com>
2022-12-30 17:26:17 +1000

    Zadacha 47

---
+++ DZSeminar7/Zadacha2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает число, 
+// и возвращает индексы числа в двумерном массиве или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] FillNewArr(int[,] NewArr)
+{
+    for (int i = 0; i < NewArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < NewArr.GetLength(1); j++)
+        {
+            NewArr[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return NewArr;
+}
+void PrintNewArr(int[,] NewArr)
+{
+    for (int i = 0; i < NewArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < NewArr.GetLength(1); j++)
+        {
+            Console.Write(NewArr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] NewArr = new int[3, 4];
+NewArr = FillNewArr(NewArr);
+PrintNewArr(NewArr);
+
+Console.Write("Введите число -> ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < NewArr.GetLength(0); i++)
+{
+    for (int j = 0; j < NewArr.GetLength(1); j++)
+    {
+        if (NewArr[i, j] == number && number < 10)
+        {
+            Console.WriteLine($"[{Convert.ToInt32(i)},{Convert.ToInt32(j)}]");
+        }
+        else
+        {
+            Console.WriteLine(number + "-> такого числа в массиве нет.");
+            break;
+        }
+    }
+}
