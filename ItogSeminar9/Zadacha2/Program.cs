0478bed3b03c9098aa3a8633d3e9d5906910cdb1
Anatoly <anatoly.shirshov1989@gmail.com>
2022-12-31 17:16:33 +1000

    Zadacha 66

--- ItogSeminar9/Zadacha2/Program.cs
+++ ItogSeminar9/Zadacha2/Program.cs
@@ -3,27 +3,14 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-Console.Write("Введите N:  ");
-int n = Int.Parse(Console.ReadLine());
+Console.Clear();
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNumbers(1, n));
 
-string PrintSum(int Number)
+int SumNumbers(int start, int end)
 {
-    if (Number == 1) return "1";
-    return (Number + PrintSum(Number - 1));
+    if (start == end) return end;
+    int answer = start + SumNumbers(start + 1, end);
+    return answer;
 }
-Console.Write(PrintNumbers(n));
-
-// Задача 2: Задайте значение N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от 1 до N.
-// M = 1; N = 15 -> 120
-
-// Console.Clear();
-// Console.Write("Введите N: ");
-// int n = int.Parse(Console.ReadLine());
-// Console.WriteLine(SumNumbers(1, n));
-
-// int SumNumbers(int start, int end)
-// {
-//     if (start == end) return end;
-//     int answer = start + SumNumbers(start + 1, end);
-//     return answer;
-// }
