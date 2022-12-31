fdb053f4acd3fe4d0f6f4520c8f528e411860511
Anatoly <anatoly.shirshov1989@gmail.com>
2022-12-31 17:19:17 +1000

    Zadacha

--- ItogSeminar9/Zadacha3/Program.cs
+++ ItogSeminar9/Zadacha3/Program.cs
@@ -3,28 +3,12 @@
 
 Console.Clear();
 Console.Write("Введите N: ");
-int n = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine(PrintNumbers(1, n));
 
 string PrintNumbers(int start, int end)
 {
     if (start == end) return end.ToString();
-    return (start + " " + PrintNumbers(start + 1, end)); // "3  2  1"
+    return (start + " " + PrintNumbers(start + 1, end)); 
 }
-
-//Задайте значение N. Напишите программу, 
-//которая выведет все натуральные числа в промежутке от 1 до N.
-
-// Console.Clear();
-// Console.Write("Введите N: ");
-// int n = int.Parse(Console.ReadLine());
-
-// Console.WriteLine(PrintNumbers(1, n));
-
-// string PrintNumbers(int start, int end)
-// {
-//     if (start == end) return end.ToString();
-//     string answer = start + " " + PrintNumbers(start + 1, end);
-//     return answer; // "3  2  1"
-// }
